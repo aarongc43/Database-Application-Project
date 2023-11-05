48775ac440112252d129ae1ac4c00306a5d36389
Jacob-Stump <jakedstump@gmail.com>
2023-11-04 21:42:23 -0500

    Update main.go
    Vendor Endpoints completed minus the authorization

--- main.go
+++ main.go
@@ -14,18 +14,91 @@ import (
 
 type NewProduct struct {
 	Category    string `json:"category"`
+	Vendor      string `json:"vendor"`
 	Name        string `json:"productName"`
 	Price       int    `json:"price"`
 	Quantity    int    `json:"quantity"`
 	Description string `json:"description"`
 }
 
-type NewProductResponse struct {
+type Vendor struct {
+	Name string `json:"vendors"`
+}
+
+type NewVendor struct {
+	Name string `json:"vendor"`
+}
+
+type CategoryNames struct {
+	Names string `json:"categories"`
+}
+
+type SuccessResponse struct {
 	Success bool   `json:"success"`
 	Error   string `json:"error,omitempty"`
 }
 
-func addProduct(w http.ResponseWriter, r *http.Request) {
+var db *sql.DB
+
+func handleVendors(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodGet {
+		rows, err := db.Query("SELECT Vendor_Name FROM vendors ORDER BY Vendor_Name;")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		var vendors []Vendor
+
+		for rows.Next() {
+			var v Vendor
+			err := rows.Scan(&v.Name)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			vendors = append(vendors, v)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(vendors)
+
+	} else if r.Method == http.MethodPost {
+		var request NewVendor
+
+		err := json.NewDecoder(r.Body).Decode(&request)
+
+		if err != nil {
+			writeJSONErrorResponse(w, http.StatusBadRequest, "Invalid JSON data")
+			return
+		}
+
+		vendorinsertstatement, err := db.Prepare("INSERT INTO vendors (Vendor_Name) values (?);")
+
+		if err != nil {
+			writeJSONErrorResponse(w, http.StatusInternalServerError, "SQL statement error")
+			return
+		}
+
+		_, err = vendorinsertstatement.Exec(&request.Name)
+
+		if err != nil {
+			writeJSONErrorResponse(w, http.StatusInternalServerError, "Product insertion error")
+			return
+		}
+
+		response := SuccessResponse{Success: true}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(response)
+
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func handleProducts(w http.ResponseWriter, r *http.Request) {
 	var request NewProduct
 
 	err := json.NewDecoder(r.Body).Decode(&request)
@@ -35,21 +108,6 @@ func addProduct(w http.ResponseWriter, r *http.Request) {
 		writeJSONErrorResponse(w, http.StatusBadRequest, "Invalid JSON data")
 		return
 	}
-
-	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "Frodobaggins123",
-		Net:    "tcp",
-		Addr:   "localhost:3306",
-		DBName: "web_store",
-	}
-
-	db, err := sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close()
 
 	getCategoryIDStatement, err := db.Prepare("SELECT Category_ID FROM categories WHERE Cat_Name = ?")
 	if err != nil {
@@ -77,14 +135,14 @@ func addProduct(w http.ResponseWriter, r *http.Request) {
 		writeJSONErrorResponse(w, http.StatusInternalServerError, "Product insertion error")
 		return
 	}
-	response := NewProductResponse{Success: true}
+	response := SuccessResponse{Success: true}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(response)
 }
 
 func writeJSONErrorResponse(w http.ResponseWriter, statusCode int, errMessage string) { //method to reduce code repetition when returning a JSON formatted error response
-	response := NewProductResponse{Success: false, Error: errMessage}
+	response := SuccessResponse{Success: false, Error: errMessage}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(response)
@@ -94,14 +152,30 @@ func handleRequest(corsMiddleware func(http.Handler) http.Handler) {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(corsMiddleware)
 
-	myRouter.HandleFunc("/addProduct", addProduct)
-	//add more endpoints and associated funcs here
+	myRouter.HandleFunc("/products", handleProducts).Methods(http.MethodGet, http.MethodPost, http.MethodDelete, http.MethodPut)
+	myRouter.HandleFunc("/vendors", handleVendors).Methods(http.MethodGet, http.MethodPost)
 	//add more endpoints and associated funcs here
 	//add more endpoints and associated funcs here
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
+
+	cfg := mysql.Config{
+		User:   "root",
+		Passwd: "Frodobaggins123",
+		Net:    "tcp",
+		Addr:   "localhost:3306",
+		DBName: "web_store",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
 
 	corsMiddleware := handlers.CORS(
 		handlers.AllowedOrigins([]string{"http://localhost:3000"}),
