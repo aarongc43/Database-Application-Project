bb873addedd06e59057d518ce0b3015030b9bc36
aarongc43 <aarongc43@gmail.com>
2023-11-20 12:19:50 -0600

    Refactored, cleaned, and organized code. Added the dollar to the price field but it is adding two dollars instead of one. Will get it fixed

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -7,6 +7,19 @@ function App() {
     
     // state hooks for the components
     const [selectedOption, setSelectedOption] = useState("Products");
+    const [selectedTab, setSelectedTab] = useState("Customers");
+    const [vendors, setVendors] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedVendor, setSelectedVendor] = useState("");
+    const [newVendor, setNewVendor] = useState({ name:""});
+    const [newCategory, setNewCategory] = useState({ name: ""});
+    const [priceInput, setPriceInput] = useState('');
+    const [isInputFocused, setIsInputFocused] = useState(false);
+    const [tableData, setTableData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    // go server is seeing this as an object
     const [newProduct, setNewProduct] = useState({
         vendor: "",
         category: "",
@@ -16,31 +29,21 @@ function App() {
         description: ""
     });
 
-    const [vendors, setVendors] = useState([]);
-    const [categories, setCategories] = useState([]);
-    const [newVendor, setNewVendor] = useState({ name:""});
-    const [newCategory, setNewCategory] = useState({ name: ""});
-    const [selectedVendor, setSelectedVendor] = useState("");
-    const [selectedTab, setSelectedTab] = useState("Customers");
+    useEffect(() => {
+        const fetchInitialData = async () => {
+            await fetchVendors();
+        };
+        fetchInitialData();
+    }, []);
 
-    // dropdown handler
-    const handleDropdown = e => setSelectedOption(e.target.value);
-
-    // input change handler for product form
-    const handleInputChange = e => {
-        const { name, value } = e.target;
-        
-        if (name === "vendor") {
-            // Call fetchCategories with the selected vendor
-            fetchCategories(value);
-            setSelectedVendor(value);
-        }
-        setNewProduct(prev => ({ ...prev, [name]: value }));
-
-        if (name === "vendor") {
-            fetchCategoriesForVendor(value);
-        }
-    };
+    useEffect(() => {
+        const fetchVendorCategories = async () => {
+            if (selectedVendor) {
+                await fetchCategories(selectedVendor);
+            }
+        };
+        fetchVendorCategories();
+    }, [selectedVendor]);
 
     const fetchCategoriesForVendor = async (vendorId) => {
         if (!vendorId) {
@@ -48,14 +51,13 @@ function App() {
             // selected
             setCategories([]);
         }
-        
         try {
             setLoading(true);
             // this needs to change
             const response = await fetch(`http://localhost:8080/categories/${newProduct.vendor}`);
             if (!response.ok) {
                 throw new Error('Failed to "fetch" categories for vendor');
-                }
+            }
             const data = await response.json();
             setCategories(data);
         } catch (error) {
@@ -65,85 +67,6 @@ function App() {
         }
     };
 
-    // API Call placeholder
-    // Expected input
-    //   category: string,
-    //   productName: string,
-    //   price: string,
-    //   quantity: string,
-    //   description: string
-    const sendProductToSQL = async (product) => {
-        
-        const username = prompt("Enter your username:");
-        const password = prompt("Enter your password:");
-
-        //console.log(username);
-        //console.log(password);
-        
-        try {
-            const url = 'http://localhost:8080/protected/products';
-    
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Basic ' + btoa(username + ':' + password), 
-                },
-                body: JSON.stringify(product), 
-            });
-    
-            if (!response.ok) {
-                const errorMessage = await response.text();
-                toast.error(`Error sending product data: ${errorMessage}`);
-            } else {
-                toast.success('Product data stored successfully!', {
-                    position: toast.POSITION.TOP_RIGHT,
-                });
-            }
-            
-        } catch (error) {
-            toast.error(`Error sending product data: ${error.message}`);
-        }
-    };
-
-    // Expected input: vendorName string
-    const addVendorToSQL = async (vendorName) => {
-        // API call to main.go to add vendor
-        // console.log might need to be replaced by the API call to the backend
-        // because it is interacting with the database directly?
-        console.log("sending vendor to SQL server:", vendorName);
-    };
-
-    
-    // Expected input: categoryName string
-    const addCategoryToSQL = async (categoryName) => {
-        // API call to main.go to add category
-        // console.log might need to be replaced by the API call to the backend
-        // because it is interacting with the database directly?
-        console.log("sending category to SQL server:", categoryName);
-    };
-    /*
-    POST  payload
-        - catergory: string
-        - vendor: string
-        - productName: string
-        - price: int
-        - quantity: int
-        - description: text
-     */
-
-    // form submission handler for products
-    const handleSubmit = () => {
-        sendProductToSQL(newProduct);
-        console.log("New Product:", newProduct);
-    }
-
-    // for managing state functions, still learning states so they may be wrong
-    const [tableData, setTableData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    // fetch data for a given table from sql database
     const fetchTableData = async (tableName) => {
         setLoading(true);
         setError(null);
@@ -152,25 +75,6 @@ function App() {
         setLoading(false);
         console.log(`Fetching data for ${tableName}`);
     };
-
-    // handles tab switching logic and data fetching based on a selected tab
-    const handleTabChange = (table) => {
-        setSelectedTab(table);
-        fetchTableData(table.toLowerCase());
-    };
-
-    // this is to load the initial vendor and category data from the database
-    
-    useEffect(() => {
-
-        const fetchInitialData = async () => {
-            await fetchVendors();
-            if (selectedVendor) {
-                await fetchCategories(selectedVendor);
-            }
-        };
-        fetchInitialData();
-    }, [selectedVendor]);
 
     // Expected output: updates the 'vendors' state with a list of current vendors
     // API call to fetch vendors from sql database
@@ -230,6 +134,110 @@ function App() {
         console.log("fetched and updated list of current categories:");
     };
 
+    //handlers
+    const handleDropdown = e => setSelectedOption(e.target.value);
+    // input change handler for product form
+    const handleInputChange = e => {
+        const { name, value } = e.target;
+
+        setNewProduct(prev => ({ ...prev, [name]: value }));
+
+        if (name === "vendor") {
+            fetchCategoriesForVendor(value);
+            setSelectedVendor(value);
+        }
+    };
+    
+    // restrict price input
+    const handlePriceInputChange = (e) => {
+        const value = e.target.value.replace(/[^\d.]/g, '');
+        setPriceInput(value);
+    };
+
+    // form submission handler for products
+    const handleSubmit = () => {
+        sendProductToSQL(newProduct);
+        console.log("New Product:", newProduct);
+    }
+
+    const handleFocus = () => {
+        setIsInputFocused(true);
+        setPriceInput(priceInput.replace(/^\$/, ''));
+    };
+
+    const handleBlur = () => {
+        setIsInputFocused(false);
+        setPriceInput(formatPriceDisplay(priceInput));
+    };
+
+    // handles tab switching logic and data fetching based on a selected tab
+    const handleTabChange = (table) => {
+        setSelectedTab(table);
+        fetchTableData(table.toLowerCase());
+    };
+
+    // submissions to backend
+    const sendProductToSQL = async (product) => {
+        
+        const username = prompt("Enter your username:");
+        const password = prompt("Enter your password:");
+
+        const santizedProduct = {
+            ...product,
+            price: product.price.replace(/[^\d.]/g, ''),
+        };
+
+        //console.log(username);
+        //console.log(password);
+        
+        try {
+            const url = 'http://localhost:8080/protected/products';
+    
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Basic ' + btoa(username + ':' + password), 
+                },
+                body: JSON.stringify(santizedProduct), 
+            });
+    
+            if (!response.ok) {
+                const errorMessage = await response.text();
+                toast.error(`Error sending product data: ${errorMessage}`);
+            } else {
+                toast.success('Product data stored successfully!', {
+                    position: toast.POSITION.TOP_RIGHT,
+                });
+            }
+            
+        } catch (error) {
+            toast.error(`Error sending product data: ${error.message}`);
+        }
+    };
+
+    // Expected input: vendorName string
+    const addVendorToSQL = async (vendorName) => {
+        // API call to main.go to add vendor
+        // console.log might need to be replaced by the API call to the backend
+        // because it is interacting with the database directly?
+        console.log("sending vendor to SQL server:", vendorName);
+    };
+
+    
+    // Expected input: categoryName string
+    const addCategoryToSQL = async (categoryName) => {
+        // API call to main.go to add category
+        // console.log might need to be replaced by the API call to the backend
+        // because it is interacting with the database directly?
+        console.log("sending category to SQL server:", categoryName);
+    };
+
+    const formatPriceDisplay = (value) => {
+        return value ? `$${value}` : '';
+    };
+
+
     return (
         <div className="app-container">
         <div className="container">
@@ -244,58 +252,81 @@ function App() {
 
             {selectedOption === "Products" && (
                 <div>
-                    <select
-                        className="input"
-                        name="vendor"
-                        value={selectedVendor}
-                        onChange={handleInputChange}
-                    >
-                        <option value="">Select Vendor</option>
-                        {Object.entries(vendors).map(([vendorId, vendorName]) => (
-                            <option key={vendorId} value={vendorName}>
-                                {vendorName}
-                            </option>
-                        ))}
-                    </select>
+                        <select
+                            className="input"
+                            name="vendor"
+                            value={selectedVendor}
+                            onChange={handleInputChange}
+                        >
+                            <option value="">Select Vendor</option>
+                            {Object.entries(vendors).map(([vendorId, vendorName]) => (
+                                <option key={vendorId} value={vendorName}>
+                                    {vendorName}
+                                </option>
+                            ))}
+                        </select>
 
-                    <select
-                        className="input"
-                        name="category"
-                        value={newProduct.category}
-                        onChange={handleInputChange}
-                        disabled={!selectedVendor} // Disable the dropdown if no vendor is selected
-                    >
-                        <option value="">Select Category</option>
-                        {categories.map(categoryName => (
-                            <option key={categoryName} value={categoryName}>
-                                {categoryName}
-                            </option>
-))}
-                    </select>
-
-                    {["productName", "price", "quantity"].map(field => (
+                        <select
+                            className="input"
+                            name="category"
+                            value={newProduct.category}
+                            onChange={handleInputChange}
+                            disabled={!selectedVendor} // Disable the dropdown if no vendor is selected
+                        >
+                            <option value="">Select Category</option>
+                            {categories.map(categoryName => (
+                                <option key={categoryName} value={categoryName}>
+                                    {categoryName}
+                                </option>
+    ))}
+                        </select>
+                    <div className="input-group">
                         <input
-                            key={field}
-                            className={field === "price" ? "inputPrice" : "input"}
+                            className="input"
                             type="text"
-                            placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
-                            name={field}
-                            value={newProduct[field]}
+                            placeholder="ProductName"
+                            name="productName"
+                            value={newProduct.productName}
                             onChange={handleInputChange}
                         />
-                    ))}
-                    <textarea
-                        className="input"
-                        placeholder="Product Description"
-                        name="description"
-                        value={newProduct.description}
-                        onChange={handleInputChange}
-                    ></textarea>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Price"
+                            name="price"
+                            value={isInputFocused ? priceInput : formatPriceDisplay(priceInput)}
+                            onChange={handlePriceInputChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                        />
+                    </div>
+                    <div className="input-group">
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="Quantity"
+                            name="quantity"
+                            value={newProduct.quantity}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="input-group">
+                            <textarea
+                                className="input"
+                                type="text"
+                                placeholder="Description"
+                                name="description"
+                                value={newProduct.description}
+                                onChange={handleInputChange}
+                            ></textarea>
+                    </div>
 
-                    <button className="button" onClick={handleSubmit}>
-                        Add Product
-                    </button>
-                </div>
+                        <button className="button" onClick={handleSubmit}>
+                            Add Product
+                        </button>
+                    </div>
             )}
 
             {selectedOption === "Vendor" && (
